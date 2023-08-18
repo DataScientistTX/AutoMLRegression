c582c7e578fa676fb9a9e0b73041164ebdbea71f
Sercan Gul | Data Scientist | DataScientistTX <sercan.gul@gmail.com>
2023-08-18 07:43:19 -0600

    fix
    fix

--- AutoRegression.py
+++ AutoRegression.py
@@ -1,78 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This notebook explains the steps to develop an Automated Supervised Machine Learning Regression program, which automatically tunes the hyperparameters and prints out the final accuracy results as a tables together with feature importance results.
-
+# This script explains the steps to develop an Automated Supervised Machine Learning Regression program, 
+# which automatically tunes the hyperparameters and prints out the final accuracy results 
+# as a table together with feature importance results.
 # Let's import all libraries.
-
 
 import pandas as pd
 import numpy as np
-
-from xgboost import XGBRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import StandardScaler
-
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import Lasso
-from sklearn.svm import SVR
-from sklearn.neural_network import MLPRegressor
-
-from sklearn.model_selection import train_test_split
-
-from sklearn.metrics import explained_variance_score
-from sklearn.metrics import max_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_squared_log_error
-from sklearn.metrics import median_absolute_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_poisson_deviance
-from sklearn.metrics import mean_gamma_deviance
-from sklearn.metrics import mean_absolute_percentage_error
-
-from itertools import repeat
+from   xgboost import XGBRegressor
+from   itertools import repeat
 import matplotlib.pyplot as plt
 
+from   sklearn.model_selection import GridSearchCV,train_test_split
+from   sklearn.preprocessing import StandardScaler
+from   sklearn.linear_model import LinearRegression,Ridge,Lasso
+from   sklearn.ensemble import RandomForestRegressor
+from   sklearn.svm import SVR
+from   sklearn.neural_network import MLPRegressor
+from   sklearn.metrics import explained_variance_score,max_error,mean_absolute_error,mean_squared_error,mean_squared_log_error,median_absolute_error,r2_score,mean_poisson_deviance,mean_gamma_deviance,mean_absolute_percentage_error
 
-# Lets import our dataset from the csv files as a dataframe.
+# Import  dataset from the csv file as a dataframe.
 
 df = pd.read_csv('data.csv')  
-
-
-# Let's take a look at dataset. I like using df.describe() function to have some statistics about each column.
-
-df.describe().T
-
 
 n = len(df.columns)
 X = df.iloc[:,0:n-1].to_numpy() 
 y = df.iloc[:,n-1].to_numpy()
 
-
 # This defines X as all the values except the last column (columns A,B,C,D,E), and y as the last column (column numbers start from zero, hence: 0 - A, 1 - B, 2 - C, 3 - D,4 - E, 5 -F).
-
 # Some algorithms provide better accuracies with the standard scaling of the input features (i.e. normalization). Let's normalize the data. 
 
 scaler = StandardScaler()
 scaler.fit(X)
 X= scaler.transform(X)
 
-
 # We have to split our dataset as train and test data. For this we can use train_test_split by sklearn.model_selection. Test size of 0.20 means that 20% of the data will be used as test data and 80% of the data will be used for training.
 
 X_train, X_test, y_train, y_test= train_test_split(X,y,test_size = 0.20)
 
-
-# We might not always want to tune the parameters of models, or only tune for some models. For this I have defined a basic input. When it is set to "True", the program will perform the tuning for all the models.
+# Set perform tuning= "True" to perform hyperparameter tuning for all the models.
 
 Perform_tuning = True
 Lassotuning, Ridgetuning, randomforestparametertuning, XGboostparametertuning, SVMparametertuning, MLPparametertuning = repeat(Perform_tuning,6)
 
-
-# Let's define the grid search function to be used with our models. The values of grid might need to be changed regarding the problem (i.e., some problems might require higher values of n_estimators, while some might require lower ranges).
+# Define the grid search function to be used with models. 
+# The values of grid might need to be changed based on the problem 
 
 def grid_search(model,grid):
     # Instantiate the grid search model
@@ -85,31 +58,26 @@ def grid_search(model,grid):
     print (grid_search.best_params_)
     return grid_search.best_params_
 
-
 # Performing Lasso parameter tuning.
-
 if Lassotuning:
     # Create the parameter grid based on the results of random search 
     grid = {
-        'alpha': [1,0.9,0.75,0.5,0.1,0.01,0.001,0.0001] , 
+        'alpha': [1,0.9,0.5,0.1,0.01,0.001,0.0001], 
         "fit_intercept": [True, False]
     }
     Lasso_bestparam = grid_search(Lasso,grid) 
-
 
 # Performing Ridge parameter tuning.
 
 if Ridgetuning:
     # Create the parameter grid based on the results of random search 
     grid = {
-        'alpha': [1,0.9,0.75,0.5,0.1,0.01,0.001,0.0001] , 
+        'alpha': [1,0.9,0.5,0.1,0.01,0.001,0.0001], 
         "fit_intercept": [True, False]
     }
     Ridge_bestparam = grid_search(Ridge,grid) 
 
-
 # Performing Random Forest parameter tuning.
-
 
 if randomforestparametertuning:
     # Create the parameter grid based on the results of random search 
@@ -119,10 +87,9 @@ if randomforestparametertuning:
         'max_features': ['auto', 'sqrt'],
         'min_samples_leaf': [1,2,3,],
         'min_samples_split': [3, 4, 5,6,7],
-        'n_estimators': [5,10,15]
+        'n_estimators': [5,10,15,40,80]
         }
     RF_bestparam = grid_search(RandomForestRegressor,grid) 
-
 
 # Performing XGBoost parameter tuning.
 
@@ -132,22 +99,19 @@ if XGboostparametertuning:
                     'gamma': [2,5],
                     'learning_rate': [0.1,0.2,0.3],
                     'max_depth': [8,10,12],
-                    'n_estimators': [5,10],
+                    'n_estimators': [5,10,20,40,80],
                     'subsample': [0.8,1],
                     'reg_alpha': [15,20],
                     'min_child_weight':[3,5]}
     XGB_bestparam = grid_search(XGBRegressor,grid) 
 
-
 # Performing SVM parameter tuning.
-
 
 #SVM Parameter Tuning----------------------------------------------------------
 if SVMparametertuning:
     grid = {'gamma': 10. ** np.arange(-5, 3),
             'C': 10. ** np.arange(-3, 3)}
     SVR_bestparam = grid_search(SVR,grid)
-
 
 # Performing MLP parameter tuning.
 
@@ -159,8 +123,8 @@ if MLPparametertuning:
         'learning_rate': ['constant','invscaling','adaptive']}
     MLP_bestparam = grid_search(MLPRegressor,grid)   
 
-
-# Now we obtained the best parameters for all the models using the training data. Let's define the error metrics that will be used in analyzing the accuracy of each model. 
+# Now we obtained the best parameters for all the models using the training data. 
+# Let's define the error metrics that will be used in analyzing the accuracy of each model. 
 
 error_metrics = (
     explained_variance_score,
@@ -173,8 +137,7 @@ error_metrics = (
     mean_poisson_deviance,
     mean_gamma_deviance,
     mean_absolute_percentage_error        
-)
-
+    )
 
 # Let's define fit_model function to predict the results, and analyze the error metrics for each model.
 
@@ -187,8 +150,8 @@ def fit_model(model,X_train, X_test, y_train, y_test,error_metrics):
         calculations.append(calc)
     return calculations
 
-
-# Provide a summary of each model and their GridSearch best parameter results. If tuning is not performed, the script will use the default values as best parameters. 
+# Provide a summary of each model and their GridSearch best parameter results. 
+# If tuning is not performed, the script will use the default values as best parameters. 
 
 try:
     trainingmodels = (
@@ -199,7 +162,7 @@ try:
         Lasso(**Lasso_bestparam),
         SVR(**SVR_bestparam),
         MLPRegressor(**MLP_bestparam)
-    )
+        )
     
 except:
     trainingmodels = (
@@ -210,17 +173,15 @@ except:
         Lasso(),
         SVR(),
         MLPRegressor()
-    )    
+        )    
 
 calculations = []
-
 
 # Below loop performes training, testing and error metrics calculations for each model. 
 
 for trainmodel in trainingmodels:
     errors = fit_model(trainmodel,X_train, X_test, y_train, y_test,error_metrics)
     calculations.append(errors)
-
 
 # Let's organize these results, and summarize them all in a dataframe. 
 
@@ -253,15 +214,15 @@ df_error["Model"] = model_names
 cols = df_error.columns.tolist() 
 cols = cols[-1:] + cols[:-1]
 df_error = df_error[cols]
-df_error = df_error.sort_values(by=['Mean squared error'],ascending=True)
-df_error = (df_error.set_index('Model')
-        .astype(float)
-        .applymap('{:,.3f}'.format))
+
+df_error = (df_error.sort_values(by=['Mean squared error'],
+                                 ascending=True)
+                    .set_index('Model')
+                    .astype(float)
+                    .applymap('{:,.3f}'.format))
 df_error.to_csv("errors.csv")
-df_error
 
-
-# Moreover, we can analyze the feature importance results using the Random Forest regressor. 
+# Analyze the feature importance results using the Random Forest regressor. 
 
 #Principal Component Analysis
 features = df.columns[:-1]
